4278d7c1cb070aab1ed75eba7c69f5cc4fc8c0d8
renanNun <renannunes400@gmail.com>
2021-11-11 15:18:54 -0300

    Criando Entity de usuário

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,64 @@
-export class User {}
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import * as bcrypt from 'bcrypt';
+
+const saltOrRounds = 10;
+
+export enum UserRole {
+    ADMIN = "admin",
+    GHOST = "ghost"
+}
+
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        name: 'name',
+        type: 'varchar',
+        length: 255,
+        nullable: false
+    })
+    name: string;
+
+    @Column({
+        name: 'email',
+        type: 'varchar',
+        length: 255,
+        unique: true,
+        nullable: false
+    })
+    email: string;
+
+    @Column({
+        name: 'password',
+        type: 'varchar',
+        length: 255,
+        nullable: false
+    })
+    _password: string;
+
+    @Column({
+        type: "enum",
+        enum: UserRole,
+        default: UserRole.GHOST
+    })
+    role: UserRole;
+
+    @UpdateDateColumn({
+        name: 'updated_at',
+        type: 'timestamptz',
+        default: () => 'CURRENT_TIMESTAMP'
+    })
+    updatedAt: Date;
+
+    get password(): string{
+        return this._password;
+    }
+
+    set password(password: string) {
+        let hash = bcrypt.hashSync(password,saltOrRounds);
+        this._password = hash;
+    }
+}
